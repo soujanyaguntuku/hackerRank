7e58c88cbccf91ad0ccaddb997d61a31de823725
Soujanya guntuku <soujanya.guntuku@Loaner-Mac-FBU-C02SY1C0GTF1.local>
2022-12-13 05:38:16 +0100

    Developmemt Add pi intf, loops programs

---
+++ src/main/java/com/code/loops/loops1.java
@@ -0,0 +1,20 @@
+package com.code.loops;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class loops1 {
+
+  public static void main(String args[]) throws IOException {
+
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+    bufferedReader.close();
+    for (int i = 1; i <= 10; i++) {
+      System.out.printf("%d x %d = %d %n", N, i, N * i);
+    }
+  }
+}
